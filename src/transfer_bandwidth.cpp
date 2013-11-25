5cba63be16a34cc81c09ab0b52a9394dedddce88
Krishnaraj Bhat <krrishnarraj@gmail.com>
2013-11-25 22:41:25 +0530

    skip out of memory cases. Adding some more tests

---
+++ src/transfer_bandwidth.cpp
@@ -0,0 +1,12 @@
+#include <clpeak.h>
+
+
+int clPeak::runTransferBandwidthTest(cl::CommandQueue &queue, cl::Program &prog, device_info_t &devInfo)
+{
+}
+
+
+
+int clPeak::runKernelLatency(cl::CommandQueue &queue, cl::Program &prog, device_info_t &devInfo)
+{
+}
