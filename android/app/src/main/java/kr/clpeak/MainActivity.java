e11308d621729ce46740616fb41d0419ae67040d
Krishnaraj Bhat <krrishnarraj@gmail.com>
2016-07-31 22:36:20 +0530

    cosmetic ui layout changes

--- android/app/src/main/java/kr/clpeak/MainActivity.java
+++ android/app/src/main/java/kr/clpeak/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     public native void setenv(String key, String value);
 
