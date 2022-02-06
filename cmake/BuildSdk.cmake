e0fa5997ae4a26991d6d7d5c593900ab9ad803bf
Krishnaraj Bhat <krrishnarraj@gmail.com>
2022-02-06 12:15:06 +0530

    [cmake] replace icd build with newer opencl sdk
    It must contain all required deps

--- cmake/BuildSdk.cmake
+++ cmake/BuildSdk.cmake
@@ -1,29 +1,38 @@
-
-set(CLHPP_ROOT "${CMAKE_CURRENT_BINARY_DIR}/clhpp_install")
+set(SDK_ROOT "${CMAKE_CURRENT_BINARY_DIR}/sdk_install")
 
 set(CMAKE_LIST_CONTENT "
   cmake_minimum_required(VERSION 3.5)
+  project(opencl_sdk)
   include(ExternalProject)
 
-  ExternalProject_add(  hpp_headers
-    PREFIX              hpp
-    GIT_REPOSITORY      https://github.com/KhronosGroup/OpenCL-CLHPP
-    GIT_TAG             master
+  ExternalProject_add(  opencl_sdk
+    GIT_REPOSITORY      https://github.com/KhronosGroup/OpenCL-SDK
+    GIT_TAG             main
     GIT_SHALLOW         1
     GIT_PROGRESS        1
     CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX=${CLHPP_ROOT}
+      -DCMAKE_INSTALL_PREFIX=${SDK_ROOT}
       -DCMAKE_BUILD_TYPE=Release
+      -DBUILD_SHARED_LIBS=OFF
       -DBUILD_DOCS=OFF
       -DBUILD_EXAMPLES=OFF
       -DBUILD_TESTS=OFF
+      -DOPENCL_SDK_BUILD_SAMPLES=OFF
+      -DOPENCL_SDK_TEST_SAMPLES=OFF
   )
 ")
 
-set(DEPS "${CMAKE_CURRENT_BINARY_DIR}/clhpp")
+set(DEPS "${CMAKE_CURRENT_BINARY_DIR}/opencl_sdk")
 set(DEPS_BUILD "${DEPS}/build")
 file(MAKE_DIRECTORY ${DEPS} ${DEPS_BUILD})
 file(WRITE "${DEPS}/CMakeLists.txt" "${CMAKE_LIST_CONTENT}")
 execute_process(WORKING_DIRECTORY "${DEPS_BUILD}" COMMAND ${CMAKE_COMMAND} -G ${CMAKE_GENERATOR} ..)
 execute_process(WORKING_DIRECTORY "${DEPS_BUILD}" COMMAND ${CMAKE_COMMAND} --build .)
 
+set(ENV{OCL_ROOT} "${SDK_ROOT}")
+
+if(UNIX)
+  set(ENV{AMDAPPSDKROOT} "${SDK_ROOT}")
+elseif(WIN32)
+  set(ENV{AMDAPPSDKROOT} "${SDK_ROOT}/lib")
+endif()
