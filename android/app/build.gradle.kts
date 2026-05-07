4e7527e64619638ecec1fd9f6c1657192d032fdf
Krishnaraj Bhat <krrishnarraj@gmail.com>
2026-05-07 17:42:10 +0530

    Remove debug signing config from build file
    Remove debug signing configuration from build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
 
